9ca13203206a1f80cc5e8864216477e5329e525c
Carlos Perez <estebanperezroso09@gmail.com>
2025-05-23 23:04:52 -0500

    creacion de entidades y configuracion de bd

---
+++ src/main/java/com/projectfinal/spring/agrosmart/agrosmart_application/AgrosmartApplication.java
@@ -0,0 +1,13 @@
+package com.projectfinal.spring.agrosmart.agrosmart_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgrosmartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgrosmartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projectfinal/spring/agrosmart/agrosmart_application/AgrosmartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectfinal.spring.agrosmart.agrosmart_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgrosmartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
